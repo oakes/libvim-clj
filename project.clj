f36a5544d04e9027c87e0f8a0ca5de74a2f7cd1f
oakes <zach@sekao.net>
2020-03-27 20:28:47 -0400

    Start 0.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libvim-clj "0.8.1"
+(defproject libvim-clj "0.8.2-SNAPSHOT"
   :description "A library that wraps libvim"
   :url "https://github.com/oakes/libvim-clj"
   :license {:name "Public Domain"
